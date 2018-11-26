1b2308c72cc7273724eada1014c5a7dfdb6f0b4c
Varsha Ravindra <varsharavindra12@gmail.com>
2018-11-26 20:44:12 +0530

    Update 10_SortRecords.php

--- 10_SortRecords.php
+++ 10_SortRecords.php
@@ -1,4 +1,7 @@
-<!--Write a PHP program to sort the student records which are stored in the database using selection sort.-->
+<!--Write a PHP program to sort the student records which are stored in the database using selection sort.
+create database weblab;
+use weblab;
+create table student(usnvarchar(10),name varchar(20),address varchar(20));-->
 
 <!DOCTYPE html>
 <html>
